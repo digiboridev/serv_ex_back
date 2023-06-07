1d08d7b09f3df51e874aaa30b1cd9866e97693fb
digiborimusik <digiborimusik@gmail.com>
2023-06-07 20:40:22 +0300

    device password refactoring

--- src/models/order/device_password.ts
+++ src/models/order/device_password.ts
@@ -1,18 +1,63 @@
 import { Schema } from "mongoose";
 
-export type DevicePassword = {
-    type: "pattern" | "numeric";
-    password?: string;
-    dimensions?: number;
-    points?: number[];
+export type DevicePatternPassword = {
+    type: "pattern";
+    dimensions: number;
+    points: number[];
 };
+
+export type DeviceNumericPassword = {
+    type: "numeric";
+    password: string;
+};
+
+export type DevicePassword = DevicePatternPassword | DeviceNumericPassword;
 
 export const DevicePasswordSchema = new Schema(
     {
-        type: { type: String, enum: ["pattern", "numeric"], required: true },
-        password: String,
-        dimensions: Number,
-        points: [Number],
+        type: {
+            type: String,
+            enum: ["pattern", "numeric"],
+            required: true,
+        },
+        dimensions: {
+            type: Number,
+            required: function (this: DevicePassword) {
+                return this.type === "pattern";
+            },
+            validate: {
+                validator: Number.isInteger,
+                message: "{VALUE} is not an integer value",
+            },
+        },
+        points: {
+            type: [
+                {
+                    type: Number,
+                    validate: [
+                        {
+                            validator: Number.isInteger,
+                            message: "{VALUE} is not an integer value",
+                        },
+                        {
+                            validator: function (this: DevicePatternPassword, value: number) {
+                                return value >= 0 && value <= this.dimensions * this.dimensions;
+                            },
+                            message: "{VALUE} is not a valid point for this pattern",
+                        },
+                    ],
+                },
+            ],
+            required: function (this: DevicePassword) {
+                return this.type === "pattern";
+            },
+        },
+        password: {
+            type: String,
+            required: function (this: DevicePassword) {
+                return this.type === "numeric";
+            },
+        },
     },
     {
         _id: false,
