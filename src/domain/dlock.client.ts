a92f10269a8dbb284c0c291f87ce4eb98e293e70
digiborimusik <digiborimusik@gmail.com>
2023-07-04 17:45:30 +0300

    doc

--- src/domain/dlock.client.ts
+++ src/domain/dlock.client.ts
@@ -1,8 +1,17 @@
 export type Lock = {
+    /** The name of the lock. */
     name: string;
+
+    /** Release the lock. */
     release: () => Promise<unknown>;
 };
 
 export interface DLockClient {
+    /**
+     * Acquire a lock with the given name.
+     * @param lockName Typically a resource name or id.
+     * @param timeout The timeout in milliseconds for the lock to be released automatically. If the lock is not released before the timeout, it will be released automatically.
+     * @returns A lock object that can be used to release the lock.
+     */
     acquireLock(lockName: string, timeout: number): Promise<Lock>;
 }
